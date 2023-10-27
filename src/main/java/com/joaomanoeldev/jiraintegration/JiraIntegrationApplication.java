4e396e629360767e670bc9a26096111016a15d05
joaomanoel <joaom_oliveira@outlook.com>
2023-10-27 20:33:48 -0300

    First commit

---
+++ src/main/java/com/joaomanoeldev/jiraintegration/JiraIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.joaomanoeldev.jiraintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaomanoeldev/jiraintegration/JiraIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomanoeldev.jiraintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
